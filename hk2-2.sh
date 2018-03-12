d7ff5cdc35e94fc87dd09752eac41f318bf476e5
AnnaKo <annakowork@gmail.com>
2018-03-12 20:45:07 +0200

    Update hk2-2.sh

--- hk2-2.sh
+++ hk2-2.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/python
 
-A = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89] 
-B = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] 
+print('Type Your Numbers - First List:')
+A = input().split()
+print('Type Your Numbers - Second List:')
+B = input().split()
 
-print "given lists are"
-print A 
-print B
+# A = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# B = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
-C = list(set(A).intersection(B)) 
-print "the result list with common elements and no duplicates is below:"
-print C
-#[1, 2, 3, 5, 8, 13]
+print("given lists are")
+print(A)
+print(B)
 
-
-  
+C = list(set(A).intersection(B))
+print("the result list with common elements and no duplicates is below:")
+print(C)
+# [1, 2, 3, 5, 8, 13]
