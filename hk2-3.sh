2c1b47b1c696efd5b08b54c4d5891d207aecabe7
AnnaKo <annakowork@gmail.com>
2018-03-12 20:48:47 +0200

    Update hk2-3.sh

--- hk2-3.sh
+++ hk2-3.sh
@@ -2,25 +2,20 @@
 
 N = int(input("N value: "))
 
+
 def chislo(N):
     L = []
     while N > 1:
-        for I in range(9, 1, -1):
-            if N % I == 0:
-                L.append(I)
-                N /= I
+        for i in range(9, 1, -1):
+            if N % i == 0:
+                L.append(i)
+                N /= i
                 break
         else:
             return '-1'
 
     return ''.join(map(str, reversed(L)))
 
-print ("smallest value for N is:")
-print (chislo(N))
 
-
-
-
-
-
-  
+print("smallest value for N is:")
+print(chislo(N))
