208b75c8213e14413532c19a15d9731a0f1582e4
AnnaKo <annakowork@gmail.com>
2018-03-13 13:59:09 +0200

    Update test_hk6.py

--- test_hk6.py
+++ test_hk6.py
@@ -1,17 +1,20 @@
-#!/usr/bin/python
+# !/usr/bin/python
+"""python script"""
+# pylint: disable=invalid-name
 
 import unittest
 import hk6
 
-class Testhk6(unittest.TestCase):
 
+class Testhk6(unittest.TestCase):
+    """class definition"""
     def setUp(self):
         """Init"""
 
     def test_hk6_json(self):
         """Test for main"""
         self.assertTrue(hk6.ako_json())
-        
+
     def test_hk6_yaml(self):
         """Test for main"""
         self.assertTrue(hk6.ako_yaml())
